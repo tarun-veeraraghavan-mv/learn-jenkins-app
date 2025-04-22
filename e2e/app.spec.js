4ac55b4558acac68c0248070cd9b5b66d6f652f2
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-04-21 18:15:14 -0700

    test stage

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -1,27 +1,33 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test("has title", async ({ page }) => {
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Learn Jenkins/);
+  await expect(page).toHaveTitle(/BUMBAKLAAAAAAATTTT!/);
 });
 
-test('has Jenkins in the body', async ({ page }) => {
-  await page.goto('/');
+test("has Jenkins in the body", async ({ page }) => {
+  await page.goto("/");
 
-  const isVisible = await page.locator('a:has-text("Learn Jenkins on Udemy")').isVisible();
+  const isVisible = await page
+    .locator('a:has-text("BUMBAKLAAAAAAATTTT!")')
+    .isVisible();
   expect(isVisible).toBeTruthy();
 });
 
-test('has expected app version', async ({ page }) => {
-  await page.goto('/');
+test("has expected app version", async ({ page }) => {
+  await page.goto("/");
 
-  const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
+  const expectedAppVersion = process.env.REACT_APP_VERSION
+    ? process.env.REACT_APP_VERSION
+    : "1";
 
   console.log(expectedAppVersion);
 
-  const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
+  const isVisible = await page
+    .locator(`p:has-text("Application version: ${expectedAppVersion}")`)
+    .isVisible();
   expect(isVisible).toBeTruthy();
 });
