6bdba7bc0d2a77933dc313b192886e57039fb4f3
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-04-21 18:19:07 -0700

    test stage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn Jenkins link', () => {
+test("renders learn Jenkins link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Jenkins/i);
+  const linkElement = screen.getByText(/BUMBAKLAAAAAAATTTT!/i);
   expect(linkElement).toBeInTheDocument();
 });
